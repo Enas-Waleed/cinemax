7214a6b133c8555f0eea5773870737cf16b0e21d
Abhay Chauhan <abhay.chauhan_1995@hotmail.com>
2016-10-13 13:05:00 +0530

    Add Movie class to store movie details

---
+++ app/src/main/java/xyz/abhaychauhan/cinebuff/cinebuff/fragment/MoviesFragment.java
@@ -0,0 +1,32 @@
+package xyz.abhaychauhan.cinebuff.cinebuff.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import xyz.abhaychauhan.cinebuff.cinebuff.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MoviesFragment extends Fragment {
+
+
+    public MoviesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
