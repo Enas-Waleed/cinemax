13d3c4af36bfdd6e737cc5a36aec769c491aac14
Abhay Chauhan <abhay.chauhan_1995@hotmail.com>
2017-02-20 23:17:55 +0530

    Add Splash Screen Activity

---
+++ app/src/main/java/xyz/abhaychauhan/cinebuff/cinebuff/activity/activity/SplashActivity.java
@@ -0,0 +1,27 @@
+package xyz.abhaychauhan.cinebuff.cinebuff.activity.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import xyz.abhaychauhan.cinebuff.cinebuff.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3600;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
